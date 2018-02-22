11f6e26e267af8e9b40764281694922186227f94
William Shreeve <shreevew@eos14.cis.gvsu.edu>
2018-02-22 16:30:35 -0500

    Added comments

--- include/functions.h
+++ include/functions.h
@@ -2,15 +2,13 @@
 #define __H__FUNCTIONS__
 
 #include<string>
-
-
-
-
+	
+	//Checks for balanced brackets and parenthesis
 	bool balancedSymbols(std::string input);
+	
+	//Turns valid infix to postfix
 	std::string iToP(std::string input);
-
-	 int prec(char c);
-
-
-
+	
+	//Helps return priority of operators
+	int prec(char c);
 #endif
